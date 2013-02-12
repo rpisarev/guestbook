440306f2cbf89a89f8391825662f8b55fcf57a7e
root <ruslan@rpisarev.org.ua>
2013-02-12 09:01:47 -1000

    Create model v 0.1

--- gba/models.py
+++ gba/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class GuBook(models.Model):
+        username = models.CharField(max_length=255)
+	email = models.EmailField()
+	homepage =  models.URLField(blank = True)
+	text = models.CharField()
+	image = ImageField(blank = True))
+	ip = models.CharField()
+	browser = models.CharField()
+	date = models.DateField()
+	def __unicode__(self):
+                return self.text
+
+        class Meta:
+                ordering = ["date"]
+
 # Create your models here.
