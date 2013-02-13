10c9e47449365731ad6427d06e86f59d07f53cf8
root <ruslan@rpisarev.org.ua>
2013-02-12 20:16:06 -1000

    dinamic media files (maybe)

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -46,7 +46,9 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/u/testing/guestbook/'
+#MEDIA_ROOT = '/home/u/testing/guestbook/'
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'img').replace('\\','/'),
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
