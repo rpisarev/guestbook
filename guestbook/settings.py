8720174f6ebb11fb1698d61c11e6ea499b933577
rpisarev <ruslan@rpisarev.org.ua>
2013-02-14 10:59:27 +0200

    Update guestbook/settings.py
    hmm...

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -49,7 +49,7 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 #MEDIA_ROOT = '/home/u/testing/guestbook/'
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'img').replace('\\','/')[0]
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'img').replace('\\','/')
 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
