1644a24e77075de9d4d081af7c599f9b0a246799
rpisarev <ruslan@rpisarev.org.ua>
2013-02-14 12:01:13 +0200

    Update guestbook/settings.py
    add django.contrib.staticfiles to INSTALLED_APPS

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
 #    'django.contrib.sessions',
 #    'django.contrib.sites',
 #    'django.contrib.messages',
-#    'django.contrib.staticfiles',
+	'django.contrib.staticfiles',
 	'gba',
 	'captcha',
     # Uncomment the next line to enable the admin:
