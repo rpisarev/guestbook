c8d6c8b03a9ba9f36f6bad493695300739dac453
rpisarev <ruslan@rpisarev.org.ua>
2013-02-14 12:07:12 +0200

    Update guestbook/settings.py

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -61,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'img').replace('\\','/')
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'st').replace('\\','/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
